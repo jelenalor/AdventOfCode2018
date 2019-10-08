4617728d519201633e395b6bec5cc1edc17e16ba
JL <jelena.cmila@gmail.com>
2019-10-08 19:06:12 -0400

    initial commit

---
+++ day1/day_1.py
@@ -0,0 +1,29 @@
+#
+
+
+with open("input.txt") as f:
+    data = f.readlines()
+    data = [int(i) for i in data]
+# print(data)
+
+# CHALLENGE 1
+# result = 0
+# for i in data:
+#     result += i
+# print(result)
+
+# CHALLENGE 2
+
+result = 0
+list_of_sums = []
+list_of_sums.append(result)
+for i in data:
+    result += i
+    if result in list_of_sums:
+        # seen already
+        print(result)
+        break
+    else:
+        list_of_sums.append(result)
+print(list_of_sums)
+print(result)
