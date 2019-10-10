8f8f51f3e61544ee0db8fd9f876b463afd401de4
JL <jelena.cmila@gmail.com>
2019-10-10 10:49:09 -0400

    update

--- day2/day_2.py
+++ day2/day_2.py
@@ -36,6 +36,66 @@ def checksum(data):
     return count2*count3
 
 # test = ['abcdef', "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"]
-print(checksum(data))
+# print(checksum(data))
+
+
+# PART 2
+test = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+
+
+def count_the_same(row1, row2):
+    num = 0
+    for i in range(len(row1)):
+        if row1[i] == row2[i]:
+            num += 1
+    return num
+
+
+def get_dict_count(data):
+    dict_res = {}
+    for i, row in enumerate(data):
+        dict_res[i] = []
+        for j in range(i+1, len(data)):
+            row1 = data[i]
+            row2 = data[j]
+            num = count_the_same(row1, row2)
+            dict_res[i].append(num)
+    return dict_res
+
+
+def find_pair(dict_res, test):
+    k = 0
+    len_ = len(test[0])-1
+    for i in dict_res.keys():
+        if len_ in dict_res[i]:
+            k = i
+            break
+    v = 0
+    for i, j in enumerate(dict_res[k]):
+        if j == len_:
+            v = i
+    l = k+v+1
+    pair = (test[k], test[l])
+    return pair
+
+
+def find_common(pair):
+    common = ""
+    for i in range(len(pair[0])):
+        if pair[0][i] == pair[1][i]:
+            common += pair[0][i]
+    return common
+
+
+def alg(data):
+    dict_count = get_dict_count(data)
+    pair = find_pair(dict_count, data)
+    common = find_common(pair)
+    return common, pair
+
+print(alg(data))
+
+
+
 
 
