15d67f96a009033dd4695f20c601e1b8031a4057
JL <jelena.cmila@gmail.com>
2019-10-11 21:15:44 -0400

    update

--- day4/day_4.py
+++ day4/day_4.py
@@ -18,8 +18,9 @@ test = ['[1518-11-01 00:00] Guard #10 begins shift',
         '[1518-11-05 00:55] wakes up]', '[1518-11-01 00:05] falls asleep']
 
 
-
 prev_guard = ""
+
+
 def extract_guard(row):
     global prev_guard
     if "Guard" in row:
@@ -28,6 +29,7 @@ def extract_guard(row):
         return g
     else:
         return prev_guard
+
 
 def my_solution(test):
     # Cleaning and sorting
@@ -93,3 +95,69 @@ def my_solution(test):
 
 g, m, r = my_solution(data)
 print(g, m, r)
+
+
+def my_solution2(test):
+    # Cleaning and sorting
+    my_dict = {"year": [], "month": [], "day": [], "hour": [], "minute": [], "text": []}
+    for i in test:
+        date = i.split("]")[0].replace("[", "")
+        text = i.split("]")[1][1:]
+
+        my_dict["year"].append(date.split("-")[0])
+        my_dict["month"].append(date.split("-")[1])
+        my_dict["day"].append(date.split("-")[2].split()[0])
+        my_dict["hour"].append(date.split("-")[2].split()[1].split(":")[0])
+        my_dict["minute"].append(date.split("-")[2].split()[1].split(":")[1])
+        my_dict["text"].append(text)
+
+    df = pd.DataFrame(my_dict)
+    df = df.sort_values(by=["year", "month", "day", "hour", "minute"])
+
+    # Extract guard number and keep in a separate column
+    df["guard"] = df["text"].apply(extract_guard)
+
+    # Keep in numpy 2D array minutes of sleep and in separate array the guards in order
+    list_of_guards = []
+    my_np = []
+
+    guard = ""
+    start = 0
+    for i in range(df.shape[0]):
+        if "asleep" in df.iloc[i]["text"]:
+            guard = df.iloc[i]["guard"]
+            start = df.iloc[i]["minute"]
+        elif "wakes" in df.iloc[i]["text"]:
+            end = df.iloc[i]["minute"]
+            my_range = range(int(start), int(end))
+            list_of_guards.append(guard)
+            my_row = [0]*60
+            for i in my_range:
+                my_row[i] = 1
+            my_np.append(my_row)
+
+    my_np = np.array(my_np)
+    list_of_guards = np.array(list_of_guards)
+
+    # to find the guard that sleeps the most
+    dict_of_sleep_times = {}
+    for g in set(list_of_guards):
+        my_list = my_np[list_of_guards == g]
+        dict_of_sleep_times[g] = my_list.sum(axis=0)
+
+    # find max and minute and guard
+    max_g = ""
+    max_m = 0
+    max_times = 0
+    for g in set(list_of_guards):
+        m_t = max(dict_of_sleep_times[g])
+        if m_t > max_times:
+            max_g = g
+            max_m = np.argmax(dict_of_sleep_times[g])
+            max_times = max(dict_of_sleep_times[g])
+
+    result = int(max_g[1:])*max_m
+    return max_g, max_m, max_times, result
+
+
+print(my_solution2(data))
